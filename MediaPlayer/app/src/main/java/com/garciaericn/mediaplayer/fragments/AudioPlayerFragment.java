1cf0f9729cee601690fec11decf82fae00ab365f
eng618 <eng618@fullsail.edu>
2014-10-06 01:25:14 -0400

    Dynamically change play/pause button.

--- MediaPlayer/app/src/main/java/com/garciaericn/mediaplayer/fragments/AudioPlayerFragment.java
+++ MediaPlayer/app/src/main/java/com/garciaericn/mediaplayer/fragments/AudioPlayerFragment.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.garciaericn.mediaplayer.R;
 
@@ -14,11 +16,12 @@ import com.garciaericn.mediaplayer.R;
  * Mobile Development BS
  * Created by ENG618-Mac on 10/5/14.
  */
-public class AudioPlayerFragment extends Fragment {
+public class AudioPlayerFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = "AudioPlayerFragment.TAG";
 
     private static String packageName;
+    private static boolean isPlaying;
     private static AudioPlayerFragment audioPlayerFragment;
 
     public AudioPlayerFragment() {
@@ -27,6 +30,7 @@ public class AudioPlayerFragment extends Fragment {
 
     public static AudioPlayerFragment newInstance(String packageName) {
         AudioPlayerFragment.packageName = packageName;
+        AudioPlayerFragment.isPlaying = true;
 
         if (audioPlayerFragment == null) {
             audioPlayerFragment = new AudioPlayerFragment();
@@ -42,8 +46,47 @@ public class AudioPlayerFragment extends Fragment {
         //Load layout
         View view = inflater.inflate(R.layout.fragment_audio_player, container, false);
 
-//        String demoVideoString = "android.resource://" + packageName + "/" + R.raw.demo;
+        // Set onClickListeners
+        ImageButton playButton = (ImageButton) view.findViewById(R.id.playButton);
+        playButton.setOnClickListener(this);
+
+        ImageButton stopButton = (ImageButton) view.findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(this);
+
+        ImageButton previousButton = (ImageButton) view.findViewById(R.id.previousButton);
+        previousButton.setOnClickListener(this);
+
+        ImageButton nextButton = (ImageButton) view.findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.playButton:
+                Log.i(TAG, "Play/Pause button pressed");
+                ImageButton playPause = (ImageButton) getView().findViewById(R.id.playButton);
+                if (AudioPlayerFragment.isPlaying) {
+                    playPause.setImageResource(R.drawable.ic_action_pause);
+                    AudioPlayerFragment.isPlaying = false;
+                } else {
+                    playPause.setImageResource(R.drawable.ic_action_play);
+                    AudioPlayerFragment.isPlaying = true;
+                }
+
+
+                break;
+            case R.id.stopButton:
+                Log.i(TAG, "Stop button pressed");
+                break;
+            case R.id.previousButton:
+                Log.i(TAG, "Previous button pressed");
+                break;
+            case R.id.nextButton:
+                Log.i(TAG, "Next button pressed");
+                break;
+        }
     }
 }
