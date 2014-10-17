6175087ec39966ec300cae32568fbe2d699cd747
eng618 <eng618@fullsail.edu>
2014-10-16 20:22:04 -0400

    Completed set up of ViewsFactory.

--- AppReviews/app/src/main/java/com/garciaericn/appreviews/widget/ReviewsWidgetViewFactory.java
+++ AppReviews/app/src/main/java/com/garciaericn/appreviews/widget/ReviewsWidgetViewFactory.java
@@ -1,8 +1,16 @@
 package com.garciaericn.appreviews.widget;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.garciaericn.appreviews.R;
+import com.garciaericn.appreviews.data.DataManager;
+import com.garciaericn.appreviews.data.Review;
+
+import java.util.ArrayList;
 
 /**
  * Full Sail University
@@ -12,32 +20,59 @@ import android.widget.RemoteViewsService;
 public class ReviewsWidgetViewFactory implements RemoteViewsService.RemoteViewsFactory {
 
     private static final String TAG = "com.garciaericn.appsreviews.widget.ReviewsWidgetViewFactory.TAG";
+    private static final int ID_CONSTANT = 0x0101010;
+    private ArrayList<Review> mReviews;
+    private Context mcontext;
+
+    public ReviewsWidgetViewFactory(Context context) {
+        mcontext = context;
+    }
 
     @Override
     public void onCreate() {
         Log.i(TAG, "onCreate entered");
-        // TODO: Get data from file, and set content
+
+        DataManager mgr = DataManager.getInstance(mcontext);
+        if (mgr != null) {
+            // Cache reviews from disk
+            mReviews = mgr.readFromDisk();
+        }
     }
 
     @Override
     public void onDataSetChanged() {
         Log.i(TAG, "onDataSetChanged entered");
+        // Heavy lifting code can go here without blocking the UI.
+        // TODO: Update data in collection
     }
 
     @Override
     public void onDestroy() {
         Log.i(TAG, "onDestroy entered");
+        mReviews.clear();
     }
 
     @Override
     public int getCount() {
         Log.i(TAG, "getCount entered");
-        return 0;
+        return mReviews.size();
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
         Log.i(TAG, "getViewAt entered");
+        // TODO: Set each view with Review at position
+        Review review = mReviews.get(position);
+
+        RemoteViews itemView = new RemoteViews(mcontext.getPackageName(), R.layout.review_item);
+
+        itemView.setTextViewText(R.id.review_title, review.getReviewTitle());
+        itemView.setTextViewText(R.id.star_rating, String.valueOf(review.getStarsEarned()) + " stars");
+        itemView.setTextViewText(R.id.review_summary, review.getReviewSummary());
+
+        Intent intent = new Intent();
+//        intent.putExtra([constant from widget provider].EXTRA_ITEM, review);
+        itemView.setOnClickFillInIntent(R.id.review_item, intent);
         return null;
     }
 
@@ -50,18 +85,18 @@ public class ReviewsWidgetViewFactory implements RemoteViewsService.RemoteViewsF
     @Override
     public int getViewTypeCount() {
         Log.i(TAG, "getViewTypeCount entered");
-        return 0;
+        return 1;
     }
 
     @Override
     public long getItemId(int position) {
         Log.i(TAG, "getItemId entered");
-        return 0;
+        return ID_CONSTANT + position;
     }
 
     @Override
     public boolean hasStableIds() {
         Log.i(TAG, "hasStableIds entered");
-        return false;
+        return true;
     }
 }
