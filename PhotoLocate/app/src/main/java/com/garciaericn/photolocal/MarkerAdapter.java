dfc0b051a7395a953d0284ffd7e5ac56df2ca558
eng618 <eng618@fullsail.edu>
2014-10-21 23:44:40 -0400

    Created Marker Adapter class.  Created stubs for listeners.

---
+++ PhotoLocate/app/src/main/java/com/garciaericn/photolocal/MarkerAdapter.java
@@ -0,0 +1,43 @@
+package com.garciaericn.photolocal;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 10/21/14.
+ */
+public class MarkerAdapter implements GoogleMap.InfoWindowAdapter {
+
+    Context mContext;
+    TextView mText;
+
+    public MarkerAdapter() {
+        if (mText == null) {
+            mText = new TextView(mContext);
+        }
+    }
+
+    public MarkerAdapter(Context context) {
+        mContext = context;
+        if (mText == null) {
+            mText = new TextView(mContext);
+        }
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        mText.setText(marker.getTitle() + " adapter working!!");
+        return mText;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+}
--- PhotoLocate/app/src/main/java/com/garciaericn/photolocal/fragments/PhotoMapFragment.java
+++ PhotoLocate/app/src/main/java/com/garciaericn/photolocal/fragments/PhotoMapFragment.java
@@ -1,15 +1,31 @@
 package com.garciaericn.photolocal.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.garciaericn.photolocal.MarkerAdapter;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Full Sail University
  * Mobile Development BS
  * Created by ENG618-Mac on 10/21/14.
  */
-public class PhotoMapFragment extends MapFragment {
+public class PhotoMapFragment extends MapFragment
+        implements
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMapLongClickListener{
 
     public static final String TAG = "PhotoMapFragment.TAG";
+    private GoogleMap googleMap;
 
     public PhotoMapFragment() {
 
@@ -17,5 +33,55 @@ public class PhotoMapFragment extends MapFragment {
 
     public static PhotoMapFragment getInstance() {
         return new PhotoMapFragment();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Get instance of map
+        googleMap = getMap();
+
+        // Add a map marker
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(28.590647, -81.304510)).title("MDVBS Faculty Offices"));
+
+        // Set marker adapter
+        googleMap.setInfoWindowAdapter(new MarkerAdapter(getActivity()));
+        // Set listeners
+        googleMap.setOnInfoWindowClickListener(this);
+        googleMap.setOnMapClickListener(this);
+        googleMap.setOnMapLongClickListener(this);
+
+
+        // Set target, zoom, and animation
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28.590647, -81.304510), 17));
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(getActivity(), marker.getTitle() + " pressed", Toast.LENGTH_SHORT).show();
+        // TODO: launch detail frag
+    }
+
+    @Override
+    public void onMapClick(final LatLng latLng) {
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Map Clicked")
+                .setMessage("Add new marker here?")
+                .setNegativeButton("No", null)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        googleMap.addMarker(new MarkerOptions().position(latLng).title("New Marker"));
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Toast.makeText(getActivity(), "LongPress at: " + latLng, Toast.LENGTH_SHORT).show();
+        // TODO: Add new marker at this point.
     }
 }
